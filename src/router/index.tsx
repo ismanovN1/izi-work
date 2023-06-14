e8de8e04c889ee37f3914171ce06f6fee39fb4a0
ismanovn1 <ismanov98q@mail.ru>
2023-06-14 11:46:22 +0500

    removed waiter from employer

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Root from 'pages/Employer/Root';
-import { createBrowserRouter, Outlet, redirect } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Home from 'pages/Employer/Home';
 import Personals from 'pages/Employer/Personals';
 import PersonalDetail from 'pages/Employer/PersonalDetail';
 import Auth from 'pages/Employer/Auth/Auth';
 import Registration from 'pages/Employer/Auth/Registration';
-import SeekerRegistration from 'pages/JoobSeeker/Auth/Registration';
-import SeekerAuth from 'pages/JoobSeeker/Auth/Auth';
 import CreateEditCompany from 'pages/Employer/CreateEditCompany';
 import Company from 'pages/Employer/Company';
 import MyVacacies from 'pages/Employer/MyVacancies';
@@ -17,19 +15,6 @@ import MyVacaciesCreate from 'pages/Employer/MyVacancies/create';
 import Vacancies from 'pages/Employer/MyVacancies/vacancies';
 import Responses from 'pages/Employer/MyVacancies/responses';
 import SearchCandidates from 'pages/Employer/SearchForCandidates';
-import JoobSeekerRootPage from 'pages/JoobSeeker/JoobSeekerRootPage';
-import { isMobile } from 'react-device-detect';
-import Main from 'pages/JoobSeeker/Main';
-import Job from 'pages/JoobSeeker/Jobs';
-import SearchJobInTheCard from 'pages/JoobSeeker/SearchJobInTheMap';
-import { vacancies } from 'data/vacancies';
-import VacancyDetail from 'pages/JoobSeeker/VacancyDetail';
-import Chat from 'pages/JoobSeeker/Chat';
-import Account from 'pages/JoobSeeker/Account';
-import CreateResume from 'pages/JoobSeeker/Resume/create-resume';
-import ResumeRoot from 'pages/JoobSeeker/Resume';
-import MyResume from 'pages/JoobSeeker/Resume/my-resume';
-import Profile from 'pages/JoobSeeker/profile';
 
 export default createBrowserRouter([
   {
@@ -94,72 +79,6 @@ export default createBrowserRouter([
       },
     ],
   },
-  // {
-  //   path: 'waiter',
-  //   element: <JoobSeekerRootPage />,
-  //   children: [
-  //     {
-  //       index: true,
-  //       element: <Main />,
-  //     },
-  //     {
-  //       path: 'auth',
-  //       element: <SeekerAuth />,
-  //     },
-  //     {
-  //       path: 'profile',
-  //       element: <Profile />,
-  //     },
-  //     {
-  //       path: 'resume',
-  //       element: <ResumeRoot />,
-  //       children: [
-  //         {
-  //           index: true,
-  //           element: <MyResume />,
-  //         },
-  //         {
-  //           path: 'create-edit',
-  //           element: <CreateResume />,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: 'account',
-  //       element: <Account />,
-  //     },
-  //     {
-  //       path: 'registr',
-  //       element: <SeekerRegistration />,
-  //     },
-  //     {
-  //       path: 'vacancies',
-  //       element: (
-  //         <>
-  //           <Outlet />
-  //         </>
-  //       ),
-  //       children: [
-  //         {
-  //           index: true,
-  //           element: <Job />,
-  //         },
-  //         {
-  //           path: ':vacancyId',
-  //           element: <VacancyDetail />,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: 'chat/:chatId',
-  //       element: <Chat />,
-  //     },
-  //     {
-  //       path: 'search-map',
-  //       element: <SearchJobInTheCard />,
-  //     },
-  //   ],
-  // },
   {
     path: 'account/auth',
     element: <Auth />,
